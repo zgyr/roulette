d93e51e055852a5f2e204bfb23ae0fd0ce5bd6b8
zgyr <0112d759@gmail.com>
2021-01-10 14:25:43 +0600

    begining

--- init.lua
+++ init.lua
@@ -1,1 +1,14 @@
+local component = require('component')
+local chr = require('unicode').char(0x2580)
+local gpu = component.gpu
+local inflate = component.data.inflate
 
+local function draw()
+
+end
+
+local function main()
+
+end
+
+main()
